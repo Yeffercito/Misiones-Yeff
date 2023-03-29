dba29dffb9e7a94b87ff727739140c55ff760376
Yeffercito <yeffermlopez@gmail.com>
2023-03-29 16:13:37 -0500

    a

--- recuperacion/main.js
+++ recuperacion/main.js
@@ -3,24 +3,49 @@ const imail = document.getElementById("imail");
 const usuario = document.getElementById("usuario");
 const invalid1 = document.getElementById("invalid1");
 const invalid2 = document.getElementById("invalid2");
-
+let comprobar = 0
 button1.addEventListener("click", function () {
-    invalid1.innerHTML=""
-    invalid2.innerHTML=""
-  datos.forEach((element) => {
-    let imaill = imail.value;
-    let usuarioo = usuario.value;
-    if (imaill == element.email) {
-      if ((usuarioo == element.username)) {
-        window.location.href = "index1.html";
+  comprobar = 0
+  invalid1.innerHTML = ""
+  invalid2.innerHTML = ""
+  let imaill = imail.value;
+  let usuarioo = usuario.value;
+
+if(imaill!==""){
+  if(usuarioo!==""){
+    datos.forEach((element) => {
+    
+      console.log(imaill);
+      console.log(element.email);
+  
+      if (imaill === element.email) {
+        comprobar = 1
+        if ((usuarioo === element.username)) {
+          window.location.href = "index1.html";
+        }
+        else {
+          invalid2.innerHTML = " <p>Usuario invalido</p>"
+          
+        }
+      } else {
+        console.log(comprobar);
+        if (comprobar == 1) {
+         
+          invalid1.innerHTML = ""
+        } else {
+          invalid1.innerHTML = "<p>Email no encontrado</p>"
+          
+        }
       }
-      else{
-        invalid2.innerHTML=" <p>Usuario invalido</p>"
-      }
-    }else{
-        invalid1.innerHTML="<p>Email no encontrado</p>"
-    }
-  });
+  
+    })
+  }else{
+    invalid2.innerHTML = " <p>Ingresar Usuario</p>"
+  }
+}else{
+  invalid1.innerHTML = "<p>Ingresar Email</p>"
+}
+
 });
 
 const main = async () => {
